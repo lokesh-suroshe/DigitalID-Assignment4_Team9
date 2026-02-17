e3b887157311f21ce0ad3bb56ff611f518c864b2
s4037298-ps1nov <s4037298@student.rmit.edu.au>
2026-02-17 17:45:00 +1100

    added logic to load citizen data from text file

--- src/main/java/com/group9/digitalid/PersonManager.java
+++ src/main/java/com/group9/digitalid/PersonManager.java
@@ -1,10 +1,20 @@
 package com.group9.digitalid;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class PersonManager {
     private static final String FILENAME = "citizens.txt";
 
     public boolean updatePersonalDetails(String oldID, String newID, String newName, 
                                         String newAddress, String newBirthdate) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(FILENAME));
+        } catch (IOException e) {
+            return false;
+        }
         return false;
     }
 }
