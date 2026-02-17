6ea826ec52782fb730b2d57573d3e5b3911d543f
vishnu21v <vishnuvaitheeswaran2002@gmail.com>
2026-02-16 23:16:59 -0800

    Added tests for invalid date format and points outside range

--- src/test/java/com/group9/digitalid/PersonTest.java
+++ src/test/java/com/group9/digitalid/PersonTest.java
@@ -13,6 +13,23 @@ class PersonDemeritTest {
        // Add valid demerit points and expect success
        assertEquals("Success", p.addDemeritPoints("10-02-2026", 3), "Should succeed with valid inputs");
    }
-   
+      @Test
+   void testInvalidDateFormat() {
+       // Create person
+       Person p = new Person("56s_d%&fAB", "15-11-1990");
+
+       // Invalid date format (slashes instead of dashes) should fail
+       assertEquals("Failed", p.addDemeritPoints("10/02/2026", 3), "Should fail due to invalid date format");
+   }
+
+   @Test
+   void testPointsOutsideRange() {
+       // Create person
+       Person p = new Person("56s_d%&fAB", "15-11-1990");
+
+       // Points outside valid range should fail
+       assertEquals("Failed", p.addDemeritPoints("10-02-2026", 7), "Should fail because points exceed 6");
+   }
+
 
 }
